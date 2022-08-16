ca82dd8381a47b600feedf8ba5de0ffa64073e74
Joya322 <jannatferdoush1212@gmail.com>
2022-08-16 15:22:36 +0600

    done

---
+++ js/querySelector.js
@@ -0,0 +1,7 @@
+//it will select only first elements of named by fruits classname
+const fruits = document.querySelector(".fruits");
+console.log(fruits);
+
+//it will select all elements of named by fruits classname
+const fruitss = document.getElementsByClassName("fruits");
+console.log(fruitss);
